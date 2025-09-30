7f85cd3ceb1308fbc27a865c98a0a96eca9eff05
Lotanna <tannaloamobi@gmail.com>
2025-09-30 22:20:00 +0100

    added function getFormData to read form elements

---
+++ src/employeeType.ts
@@ -0,0 +1,10 @@
+export type EmployeeData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  mobile: string;
+  address: string;
+  department: string;
+  startDate: string;
+  contractType: "Permanent" | "Contract";
+};
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,33 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import "./style.scss"
+import "./style.scss";
+import type { EmployeeData } from "./employeeType";
+
+const form = document.getElementById("employeeFormSection") as HTMLFormElement;
+const createEmployeeBtn = document.getElementById(
+  "createEmployeeBtn"
+) as HTMLButtonElement;
+
+const displayForm = () => {
+  form.style.display = "block";
+};
+
+const getFormData = (form: HTMLFormElement): EmployeeData => {
+  const formInfo = new FormData(form);
+
+  return {
+    firstName: formInfo.get("employeeFirstName") as string,
+    lastName: formInfo.get("employeeLastName") as string,
+    email: formInfo.get("employeeEmail") as string,
+    mobile: formInfo.get("employeeMobile") as string,
+    address: formInfo.get("employeeAddress") as string,
+    department: formInfo.get("employeeDept") as string,
+    startDate: formInfo.get("startDate") as string,
+    contractType: formInfo.get("contractType") as any,
+  };
+};
+
+
+createEmployeeBtn.addEventListener("click", displayForm);
 
